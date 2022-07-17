d20699a4f196d84088e783de3cab7ca2641c365d
Vlad Emelianov <volshebnyi@gmail.com>
2022-07-17 16:07:03 +0300

    Initial commit

---
+++ scripts/before_commit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT_PATH=$(dirname $(dirname $0))
+cd $ROOT_PATH
+
+npx pyright s3transfer-stubs
+flake8 s3transfer-stubs
+black s3transfer-stubs
+isort s3transfer-stubs
+mypy s3transfer-stubs
