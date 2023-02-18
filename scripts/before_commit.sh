5d07bb85ea6126ca2e58787ce3a47dbec7327e6b
Vlad Emelianov <volshebnyi@gmail.com>
2023-02-18 15:03:19 +0300

    Update CI

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,9 +4,9 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd $ROOT_PATH
 
-npx pyright s3transfer-stubs
-flake8 s3transfer-stubs
-black s3transfer-stubs
-isort s3transfer-stubs
-mypy s3transfer-stubs
-python -m istub -u
+poetry run npx pyright s3transfer-stubs
+poetry run flake8 s3transfer-stubs
+poetry run black s3transfer-stubs
+poetry run isort s3transfer-stubs
+poetry run mypy s3transfer-stubs
+poetry run python -m istub -u
