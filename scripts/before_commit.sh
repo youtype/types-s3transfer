7cbfd8fa2f901725f8c75449597edbe3870f8b12
Vlad Emelianov <volshebnyi@gmail.com>
2024-08-27 06:04:09 +0300

    Use uv instead of poetry

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -4,9 +4,5 @@ set -e
 ROOT_PATH=$(dirname $(dirname $0))
 cd $ROOT_PATH
 
-poetry run npx pyright s3transfer-stubs
-poetry run flake8 s3transfer-stubs
-poetry run black s3transfer-stubs
-poetry run isort s3transfer-stubs
-poetry run mypy s3transfer-stubs
-poetry run python -m istub -u
+uv run pre-commit run --all-files
+uv run istub -u
